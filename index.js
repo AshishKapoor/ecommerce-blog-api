587c242935e37433afd75b514fddac44e58c1c1e
Ashish Kapoor <swiftobjc@gmail.com>
2018-12-29 22:33:32 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,14 +1,56 @@
+#!/usr/bin/env node
+
 import express from 'express';
-import routes from './src/routes/crmRoutes';
 import bodyParser from 'body-parser';
-import mongoose from 'mongoose';
+import routes from './src/routes/crmRoutes';
+
+const Sequelize = require('sequelize');
 
 const app = express();
 const PORT = 3000;
 
-// mongoose connection
-mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost/CRMdb');
+const sequelize = new Sequelize('ecommerce', 'ashishkapoor', null, {
+  host: 'localhost',
+  dialect: 'postgres',
+  operatorsAliases: false,
+  pool: {
+    max: 5,
+    min: 0,
+    acquire: 30000,
+    idle: 10000
+  },
+});
+
+sequelize
+  .authenticate()
+  .then(() => console.log('Connection has been established successfully.'))
+  .catch(err => console.error('Unable to connect to the database:', err));
+
+  // const User = sequelize.define('user', {
+  //   firstName: {
+  //     type: Sequelize.STRING
+  //   },
+  //   lastName: {
+  //     type: Sequelize.STRING
+  //   }
+  // });
+  
+  // force: true will drop the table if it already exists
+  // User.sync({force: true}).then(() => {
+  //   // Table created
+  //   return User.create({
+  //     firstName: 'Master',
+  //     lastName: 'Williams'
+  //   });
+  // });
+
+  // User.findAll().then(users => {
+  //   console.log(users)
+  // })
+
+  // User.findOne().then(user => {
+  //   console.log(user.get('firstName'));
+  // });
 
 // bodyparser setup
 app.use(bodyParser.urlencoded({
