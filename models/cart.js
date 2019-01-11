40028120d92fff356c85f27215c4911f95a7bc14
Ashish Kapoor <swiftobjc@gmail.com>
2019-01-11 23:02:14 +0530

    Create cart.js

---
+++ models/cart.js
@@ -0,0 +1,15 @@
+module.exports = (sequelize, DataTypes) => {
+    const Cart = sequelize.define('cart', {
+        id: {
+          type: DataTypes.INTEGER,
+          primaryKey: true,
+          autoIncrement: true
+        },
+      },
+      {
+        freezeTableName: true,
+      }
+    );
+  
+    return Cart;
+  }
