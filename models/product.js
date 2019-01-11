8ad89154b33e74ff59fc28689a864bcc325fa218
Ashish Kapoor <swiftobjc@gmail.com>
2019-01-11 23:02:18 +0530

    Create product.js

---
+++ models/product.js
@@ -0,0 +1,18 @@
+module.exports = (sequelize, DataTypes) => {
+    const Product = sequelize.define('product', {
+        id: {
+          type: DataTypes.INTEGER,
+          primaryKey: true,
+          autoIncrement: true
+        },
+        name: DataTypes.STRING,
+        price: DataTypes.STRING,
+        available: DataTypes.INTEGER,
+      },
+      {
+        freezeTableName: true,
+      }
+    );
+  
+    return Product;
+  }
