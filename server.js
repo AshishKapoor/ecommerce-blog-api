1cb2ac6155a20d40138df4066a0e87efe897c5f8
Ashish Kapoor <swiftobjc@gmail.com>
2019-06-25 00:31:27 +0530

    Add vscode debugger,Add comments and refactor.

--- server.js
+++ server.js
@@ -1,8 +1,5 @@
 const express = require("express");
 const bodyParser = require("body-parser");
-const faker = require("faker");
-const times = require("lodash.times");
-const random = require("lodash.times");
 const db = require("./models");
 const apiPost = require("./app/api/post");
 const apiAuthor = require("./app/api/author");
@@ -25,20 +22,5 @@ apiLogin(app, db);
 apiOrder(app, db);
 
 db.sequelize.sync().then(() => {
-  // populate author table with dummy data
-  db.author.bulkCreate(
-    times(10, () => ({
-      firstName: faker.name.firstName(),
-      lastName: faker.name.lastName()
-    }))
-  );
-  // populate post table with dummy data
-  db.post.bulkCreate(
-    times(10, () => ({
-      title: faker.lorem.sentence(),
-      content: faker.lorem.paragraph(),
-      authorId: random(1, 10)
-    }))
-  );
   app.listen(8080, () => console.log("App listening on port 8080!"));
 });
