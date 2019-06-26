1cd65bcb3fb80a14b2384942eeaf7ff7f01719e7
Ashish Kapoor <swiftobjc@gmail.com>
2019-06-26 15:17:24 +0530

    Updated packages.

--- app/api/login.js
+++ app/api/login.js
@@ -1,8 +1,8 @@
 const { JWT_PRIVATE_KEY } = require('../../config/config');
 const jwt = require('jsonwebtoken');
 const bcrypt = require('bcrypt');
-const cookieParser = require('cookie-parser');
-const session = require('express-session');
+// const cookieParser = require('cookie-parser');
+// const session = require('express-session');
 
 module.exports = (app, db) => {
   // https://www.codementor.io/mayowa.a/how-to-build-a-simple-session-based-authentication-system-with-nodejs-from-scratch-6vn67mcy3
