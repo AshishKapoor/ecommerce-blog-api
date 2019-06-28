3670ac030c6c110c1113bf8236d13005a85087a8
Ashish Kapoor <swiftobjc@gmail.com>
2019-06-28 09:26:07 +0530

    Updates

--- routes/order.js
+++ routes/order.js
@@ -1,42 +1,42 @@
 const checkAuth = require('../middleware/check-auth');
 
 module.exports = (app, db) => {
-    app.get( "/orders", checkAuth, (_, res) =>
-      db.order.findAll().then( (result) => res.json(result) )
-    );
-  
-    app.get( "/order/:id", (req, res) =>
-      db.order.findByPk(req.params.id).then( (result) => res.json(result))
-    );
-  
-    app.post( "/order", checkAuth, (req, res) => 
-      db.order.create({
-        title: req.body.title,
-        content: req.body.content
-      })
-      .then( (result) => res.json(result))
-      .catch( (err) => {
-        throw res.json(err.errors.map(msg => msg.message));
-      })
-    );
-  
-    app.put( "/order/:id", (req, res) =>
-      db.order.update({
-        title: req.body.title,
-        content: req.body.content
-      },
-      {
-        where: {
-          id: req.params.id
-        }
-      }).then( (result) => res.json(result) )
-    );
-  
-    app.delete( "/order/:id", (req, res) =>
-      db.order.destroy({
-        where: {
-          id: req.params.id
-        }
-      }).then( (result) => res.json(result) )
-    );
-  }
+  app.get( "/orders", checkAuth, (_, res) =>
+    db.order.findAll().then( (result) => res.json(result) )
+  );
+
+  app.get( "/order/:id", (req, res) =>
+    db.order.findByPk(req.params.id).then( (result) => res.json(result))
+  );
+
+  app.post( "/order", checkAuth, (req, res) => 
+    db.order.create({
+      title: req.body.title,
+      content: req.body.content
+    })
+    .then( (result) => res.json(result))
+    .catch( (err) => {
+      throw res.json(err.errors.map(msg => msg.message));
+    })
+  );
+
+  app.put( "/order/:id", (req, res) =>
+    db.order.update({
+      title: req.body.title,
+      content: req.body.content
+    },
+    {
+      where: {
+        id: req.params.id
+      }
+    }).then( (result) => res.json(result) )
+  );
+
+  app.delete( "/order/:id", (req, res) =>
+    db.order.destroy({
+      where: {
+        id: req.params.id
+      }
+    }).then( (result) => res.json(result) )
+  );
+}
